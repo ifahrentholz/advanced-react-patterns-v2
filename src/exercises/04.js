13923ce62620126eee2a3a41ebaa2f23d66a1ff8
ifahrentholz <ingof.work@gmail.com>
2018-09-30 20:12:22 +0200

    Finalize lesson 04

--- src/exercises/04.js
+++ src/exercises/04.js
@@ -1,48 +1,65 @@
-// render props
-
 import React from 'react'
 import {Switch} from '../switch'
 
-// we're back to basics here. Rather than compound components,
-// let's use a render prop!
+function ToggleChild({on, toggle}) {
+  return (
+    <div>
+      {on ? 'The button is on' : 'The button is off'}
+      <Switch on={on} onClick={toggle} />
+      <hr />
+      <button aria-label="custom-button" onClick={toggle}>
+        {on ? 'Ausschalten' : 'Einschalten'}
+      </button>
+    </div>
+  )
+}
+
 class Toggle extends React.Component {
-  state = {on: false}
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      on: false
+    }
+  }
+
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => {
         this.props.onToggle(this.state.on)
-      },
+      }
     )
+
+  /**
+   * @returns {{on: boolean, toggle: (function(): void)}}
+   */
+  getStateAndHelpers() {
+    return {on: this.state.on, toggle: this.toggle}
+  }
+
+  /**
+   * @returns {*}
+   */
   render() {
-    const {on} = this.state
-    // We want to give rendering flexibility, so we'll be making
-    // a change to our render prop component here.
-    // You'll notice the children prop in the Usage component
-    // is a function. 🐨 So you can replace this with a call this.props.children()
-    // But you'll need to pass it an object with `on` and `toggle`.
-    return <Switch on={on} onClick={this.toggle} />
+    return this.props.children(this.getStateAndHelpers())
   }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
-function Usage({
-  onToggle = (...args) => console.log('onToggle', ...args),
-}) {
+function Usage({onToggle = (...args) => console.log('onToggle', ...args)}) {
   return (
     <Toggle onToggle={onToggle}>
-      {({on, toggle}) => (
+      {/* {({on, toggle}) => (
         <div>
           {on ? 'The button is on' : 'The button is off'}
           <Switch on={on} onClick={toggle} />
           <hr />
           <button aria-label="custom-button" onClick={toggle}>
-            {on ? 'on' : 'off'}
+            {on ? 'Ausschalten' : 'Einschalten'}
           </button>
         </div>
-      )}
+      )} */}
+      {props => <ToggleChild {...props} />}
     </Toggle>
   )
 }
